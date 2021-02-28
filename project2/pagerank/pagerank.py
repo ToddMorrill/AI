32cdd76a8289a16e5b0a55b2f856fb1fa5866bea
ToddMorrill <tmorrill12@gmail.com>
2021-02-28 13:15:32 -0500

    sample PageRank implemented

--- project2/pagerank/pagerank.py
+++ project2/pagerank/pagerank.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import os
 import random
 import re
@@ -9,21 +10,7 @@ TOLERANCE = 1e-6
 MAX_ITER = 100
 
 
-def main():
-    if len(sys.argv) != 2:
-        sys.exit("Usage: python pagerank.py corpus")
-    corpus = crawl(sys.argv[1])
-    ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
-    print(f"PageRank Results from Sampling (n = {SAMPLES})")
-    for page in sorted(ranks):
-        print(f"  {page}: {ranks[page]:.4f}")
-    ranks = iterate_pagerank(corpus, DAMPING)
-    print(f"PageRank Results from Iteration")
-    for page in sorted(ranks):
-        print(f"  {page}: {ranks[page]:.4f}")
-
-
-def crawl(directory):
+def crawl(directory: str) -> dict:
     """
     Parse a directory of HTML pages and check for links to other pages.
     Return a dictionary where each key is a page, and values are
@@ -42,15 +29,13 @@ def crawl(directory):
 
     # Only include links to other pages in the corpus
     for filename in pages:
-        pages[filename] = set(
-            link for link in pages[filename]
-            if link in pages
-        )
+        pages[filename] = set(link for link in pages[filename]
+                              if link in pages)
 
     return pages
 
 
-def transition_model(corpus, page, damping_factor):
+def transition_model(corpus: dict, page: str, damping_factor: float) -> dict:
     """
     Return a probability distribution over which page to visit next,
     given a current page.
@@ -59,10 +44,47 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+    uniform_prob = 1 / len(corpus)
+
+    # if empty set, uniform dist. over all pages
+    if not corpus[page]:
+        transition_probs = {key: uniform_prob for key in corpus}
+        return transition_probs
+
+    # every node is assigned uniform * (1 - damping_factor) prob
+    dampened_uniform_prob = uniform_prob * (1 - damping_factor)
+    transition_probs = {key: dampened_uniform_prob for key in corpus}
+
+    # neighbors are given additional damping_factor / len(neighbors) prob
+    out_degree = len(corpus[page])
+    additional_weight = damping_factor / out_degree
+    for neighbor in corpus[page]:
+        transition_probs[neighbor] += additional_weight
+
+    return transition_probs
 
 
-def sample_pagerank(corpus, damping_factor, n):
+def _sample_states(corpus: dict, damping_factor: float, n: int) -> Counter:
+    # keep track of state counts
+    counter = Counter()
+
+    # choose random starting page
+    page = random.choice(list(corpus.keys()))
+    counter[page] += 1
+    transition_probs = transition_model(corpus, page, damping_factor)
+
+    # sample the remaining n-1 states
+    for _ in range(n - 1):
+        page = random.choices(list(transition_probs.keys()),
+                              list(transition_probs.values()),
+                              k=1)[0]
+        counter[page] += 1
+        transition_probs = transition_model(corpus, page, damping_factor)
+
+    return counter
+
+
+def sample_pagerank(corpus: dict, damping_factor: float, n: int) -> dict:
     """
     Return PageRank values for each page by sampling `n` pages
     according to transition model, starting with a page at random.
@@ -71,7 +93,24 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    # sample n states
+    state_counts = _sample_states(corpus, damping_factor, n)
+
+    # warn if not all states were sampled
+    missing_keys = set(corpus.keys()) - set(state_counts.keys())
+    if missing_keys:
+        print(
+            'WARNING: Not all corpus states were sampled. Consider increasing '
+            'the number of sampled states \'n\'.')
+        for key in missing_keys:
+            state_counts[key] = 0.0
+
+    # normalize counts and sort in descending order
+    pagerank_dist = {
+        key: val / n
+        for key, val in sorted(state_counts.items(), key=lambda x: x[1])
+    }
+    return pagerank_dist
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -84,6 +123,20 @@ def iterate_pagerank(corpus, damping_factor):
     PageRank values should sum to 1.
     """
     raise NotImplementedError
+
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python pagerank.py corpus")
+    corpus = crawl(sys.argv[1])
+    ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
+    print(f"PageRank Results from Sampling (n = {SAMPLES})")
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
+    ranks = iterate_pagerank(corpus, DAMPING)
+    print(f"PageRank Results from Iteration")
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
 
 
 if __name__ == "__main__":
--- project2/pagerank/test_pagerank.py
+++ project2/pagerank/test_pagerank.py
@@ -1,26 +1,95 @@
+import math
+from re import S
+from networkx.algorithms.link_analysis.pagerank_alg import pagerank
+
 import pytest
 import networkx
 
-from pagerank import crawl, sample_pagerank, iterate_pagerank
+from pagerank import _sample_states, crawl, transition_model, sample_pagerank, iterate_pagerank
 from pagerank import DAMPING, SAMPLES, TOLERANCE, MAX_ITER
 
+
+@pytest.fixture(scope='module', params=['corpus0', 'corpus1', 'corpus2'])
+def corpus(request):
+    return crawl(request.param)
+
+
 @pytest.fixture(scope='module')
-def expected(corpus):
-    g = networkx.DiGraph()
-    g.add_nodes_from(corpus)
-    # g.add_edges_from()
-    expected = networkx.pagerank(g)
-    return expected
+def toy_corpus():
+    return {
+        '1.html': {'2.html', '3.html'},
+        '2.html': {'3.html'},
+        '3.html': {'2.html'}
+    }
 
-def test_sample_pagerank(corpus, expected):
-    corpus = crawl(corpus)
-    ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
-    assert ranks == expected
-    
 
-def test_iterate_pagerank(corpus):
-    corpus = crawl(corpus)
-    ranks = iterate_pagerank(corpus, DAMPING)
-    print(f"PageRank Results from Iteration")
-    for page in sorted(ranks):
-        print(f"  {page}: {ranks[page]:.4f}")
+def test_transition_no_neighbors():
+    # NB: recursion doesn't have any outbound links
+    sample_corpus = crawl('corpus2')
+    probs = transition_model(sample_corpus, 'recursion.html', DAMPING)
+    # assert all keys the same
+    assert set(sample_corpus.keys()) == set(probs.keys())
+    # assert uniform distribution
+    uniform_prob = 1 / len(sample_corpus)
+    assert all(val == uniform_prob for val in probs.values())
+
+
+def test_transition_simple(toy_corpus):
+    page = '1.html'
+    # assuming damping factor 0.85
+    expected = {'1.html': 0.05, '2.html': 0.475, '3.html': 0.475}
+    result = transition_model(toy_corpus, page, damping_factor=0.85)
+    assert result == expected
+
+
+@pytest.mark.parametrize('damping_factor', [0.75, 0.85])
+def test_transition_sum(damping_factor, toy_corpus):
+    # arbitrarily choose the first key from the corpus for testing
+    page = list(toy_corpus.keys())[0]
+    result = transition_model(toy_corpus, page, damping_factor)
+    sums_to_one = math.isclose(sum(result.values()), 1.0, rel_tol=1e-6)
+    assert sums_to_one
+
+
+def test_sample_states(toy_corpus):
+    state_counts = _sample_states(toy_corpus, DAMPING, SAMPLES)
+    # ensure the correct number of samples made it into the counter dict
+    assert sum(state_counts.values()) == SAMPLES
+
+
+def test_sample_pagerank_sum(corpus):
+    sample_dist = sample_pagerank(corpus, DAMPING, SAMPLES)
+    # ensure the distribution sums to 1
+    assert sum(sample_dist.values()) == 1.0
+
+
+def test_sample_pagerank_keys(toy_corpus):
+    sample_dist = sample_pagerank(toy_corpus, DAMPING, 2)
+    # ensure all keys are present
+    assert set(sample_dist.keys()) == set(toy_corpus.keys())
+
+
+@pytest.fixture(scope='module')
+def expected_dist(corpus):
+    g = networkx.DiGraph(corpus)
+    true_dist = networkx.pagerank(g, alpha=DAMPING)
+    return corpus, true_dist
+
+
+def test_sample_pagerank(expected_dist):
+    corpus, true_dist = expected_dist
+    # need A LOT of samples to converge, and still getting some chatter
+    sample_dist = sample_pagerank(corpus, DAMPING, 15000)
+    close = []
+    for key in sample_dist:
+        close.append(
+            math.isclose(sample_dist[key], true_dist[key], rel_tol=1e-1))
+    assert all(close)
+
+
+# def test_iterate_pagerank(corpus):
+#     corpus = crawl(corpus)
+#     ranks = iterate_pagerank(corpus, DAMPING)
+#     print(f'PageRank Results from Iteration')
+#     for page in sorted(ranks):
+#         print(f'  {page}: {ranks[page]:.4f}')
